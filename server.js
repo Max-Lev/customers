888d11eb524e3c072199127042582085cf891819
Max-Lev <maxlevtov@gmail.com>
2018-02-04 20:05:36 +0200

    deploy

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
