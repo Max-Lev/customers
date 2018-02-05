e8c941260c14c67086d78430fc0136069130c6cb
Max-Lev <maxlevtov@gmail.com>
2018-02-06 01:08:06 +0200

    heroku refresh router

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ var app = express();
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
 
+app.get('*', function (req, res) {
+    res.sendfile(__dirname + '/dist/index.html');
+});
+
 // Initialize the app.
 var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
 });
- 
+
